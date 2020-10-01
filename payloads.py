509ea6069361b25f932ed8960811af2d2a73a2fd
Bob Belderbos <bobbelderbos@gmail.com>
2020-10-01 06:29:27 +0200

    initial code

---
+++ payloads.py
@@ -0,0 +1,36 @@
+test_tips = [
+    {'code': ">>> names = 'bob julian tim sara'.split()\r\n"
+            ">>> ages = '11 22 33 44'.split()\r\n"
+            '>>> dict(zip(names, ages))\r\n'
+            "{'bob': '11', 'julian': '22', 'tim': '33', 'sara': '44'}",
+    'id': 9,
+    'image_link': 'https://pybites-tips.s3.eu-central-1.amazonaws.com/python-dict-zip.jpeg',
+    'link': 'https://stackoverflow.com/a/209854',
+    'share_link': 'https://twitter.com/pybites/status/1101409984104009730',
+    'tip': 'Create a dict of two sequences using the zip builtin:',
+    'title': 'make a dict using zip'},
+    {'code': ">>> names = 'bob julian tim sara'.split()\r\n"
+            '>>> names\r\n'
+            "['bob', 'julian', 'tim', 'sara']",
+    'id': 8,
+    'image_link': None,
+    'link': None,
+    'share_link': 'https://twitter.com/pybites/status/1111157389317808129',
+    'tip': 'An easy way to make a list in #Python is to use split() on a string '
+            'which splits on space by default:',
+    'title': 'split a string into a list'},
+    {'code': '>>> a = 1\r\n'
+            '>>> b = 2\r\n'
+            '>>> a, b = b, a\r\n'
+            '>>> a\r\n'
+            '2\r\n'
+            '>>> b\r\n'
+            '1',
+    'id': 7,
+    'image_link': None,
+    'link': None,
+    'share_link': 'https://twitter.com/pybites/status/1098604483658088448',
+    'tip': 'Need to swap a 2 variables in #Python? No problemo, just 1 line of '
+            'code:',
+    'title': 'swap 2 variables'}
+]
